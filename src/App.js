d86269bbf6f8a284fbd5773373ffb14843afcb6c
IvanKahinga <ikahinga@gmail.com>
2024-01-18 13:55:09 +0300

    happy bday

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import logo from "./logo.svg";
+import "./App.css";
+import HappyBirthday from "./components/HappyBirthday";
+import HeroSlider from "./components/pages/HeroSlider";
+import cake from "./assets/cake.png";
+import loons from "./assets/loons.png";
+import confetti from "./assets/confetti.png";
+
+function App() {
+  return (
+    <div className="birthdayCard">
+    <div className="cardFront">
+      <div className="front-text ">
+      <h3 className="happy">HAPPY</h3>
+      <h2 className="bday">BIRTHDAY</h2> 
+      <h3 className="toyou">WAMBUI!</h3>  
+      </div>
+      <div className="wrap-deco z-10">
+    <div className="decorations"></div>
+    <div className="decorationsTwo"></div>
+      </div> 
+        <div className="wrap-decoTwo z-10">
+    <div className="decorations"></div>
+    <div className="decorationsThree"></div>
+      </div>
+      <div className=" absolute w-[150px] h-[5px] left-[53px] top-[100px]">
+        <img alt="" src={cake}/>
+       
+      </div>
+      {/* <div className="deco absolute w-[140px] w-[40%] h-[5px] left-[1px] top-[112px]">
+        <img alt="" src={loons}/>
+       
+      </div>
+      <div className=" deco absolute w-[140px] w-[40%] h-[5px] left-[150px] top-[110px] scale-x-[-1]">
+        <img alt="" src={loons}/>
+       
+      </div> */}
+      {/* <div className="deco z-0 absolute w-[140px] h-[5px] left-[-50px] top-[270px] ">
+        <img alt="" src={confetti}/>
+       
+      </div> */}
+
+    </div>
+      
+      <div className="cardInside">
+         <div className="inside-text text-md">
+      <h3 className="happy">MO LIFE</h3>
+      <h2 className="bday">MO BLESSINGS</h2> 
+      <h3 className="toyou"></h3>  
+      </div>
+        <div className="wishes text-md font-kalam">
+        <p>Happy birthday sis!,</p>
+        <p> I hope your day is filled with lots of love and laughter! Even though we don't always see eye 2 eye, know that i'm here, cheering you on and sending lots of love your way! May all of your wishes come true.</p>
+        <p className="name font-dancing">bro</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
